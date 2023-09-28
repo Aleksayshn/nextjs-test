20211fc0799c2e9e96c966aac2e30d1c0daa122d
Oleksandra Tiankina <ag7756862@gmail.com>
2023-09-28 16:52:16 +0300

    init template

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { ExampleSection } from '@/views/ExampleSection';
+import { Countdown } from '@/components/ui/Countdown';
+
+export default function Home() {
+  return (
+    <>
+      <Countdown />
+      <ExampleSection />
+    </>
+  );
+}
